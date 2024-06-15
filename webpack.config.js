323d688ff8d239277efbbbd6bf05e10427b37b68
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-06-15 18:52:13 +0200

    add babel to webpack config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
