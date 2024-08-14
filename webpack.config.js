b8a6297ab451bfa24944691c746557d40f88328f
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-08-14 18:38:02 +0200

    Add note to review merge next project

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  //Can be changed to "production" for further optimisations
+  //Review Webpack-merge next project https://webpack.js.org/guides/production/
   mode: "development",
   entry: "./src/index.js",
   output: {
